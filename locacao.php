810164626f5ec8668ce81ec2ad7163a7f06b4dd2
paolasensato <sensatopaola@gmail.com>
2022-05-02 21:33:28 -0300

    termino da atividade

--- locacao.php
+++ locacao.php
@@ -24,7 +24,7 @@
         $data = date('Y-m-d');
         //recuperar as datas digitadas
         $datainicial = $_POST["datainicial"] ?? $data;
-        $datafinal = $_POST["datafinal"] ?? NULL;
+        $datafinal = $_POST["datafinal"] ?? $data;
     ?>
     <p> Intervalo de datas: <?=$datainicial?> e <?=$datafinal?></p>
     <table>
@@ -36,8 +36,44 @@
         </thead>
         <tbody>
             <?php
-                $sql = "select l.id, c.nome, date_format(l.data, '%d-%m-%Y) data from locacao l
-                    inner join cliente c on (c.id = l.cliente_id)";
+                $sql = "select l.id, c.nome, date_format(l.data, '%d-%m-%Y') data from locacao l
+                    inner join cliente c on (c.id = l.cliente_id)
+                    where l.data between :datainicial and :datafinal
+                    order by l.data desc";
+                    $consultaLocacao = $pdo->prepare($sql);
+                    $consultaLocacao->bindParam(":datainicial", $datainicial);
+                    $consultaLocacao->bindParam(":datafinal", $datafinal);
+                    $consultaLocacao->execute();
+                    while ($locacao = $consultaLocacao->fetch(PDO::FETCH_OBJ)) {
+                        //separar os dados 
+                        $id = $locacao->id;
+                        $data = $locacao->data;
+                        $nome = $locacao->nome;
+                        ?>
+                            <tr>
+                                <td><?=$id?></td>
+                                <td><?=$nome?></td>
+                                <td><?=$data?></td>
+                                <td>
+                                    <?php
+                                    $sql= "select f.titulo from filme_locacao fl
+                                    inner join filme f on (f.id = fl.filme_id)
+                                    where fl.locacao_id = :id 
+                                    order by f.titulo";
+                                    $consultaFilmes = $pdo->prepare($sql);
+                                    $consultaFilmes->bindParam(":id", $id);
+                                    $consultaFilmes->execute();
+                                    while ($filmes = $consultaFilmes->fetch(PDO::FETCH_OBJ)){
+                                        $titulo = $filmes->titulo;
+                                        ?>
+                                        <p><?=$titulo?></p>
+                                        <?php
+                                    }
+                                    ?>
+                                </td>
+                            </tr>
+                        <?php
+                    }
             ?>
         </tbody>
     </table>
